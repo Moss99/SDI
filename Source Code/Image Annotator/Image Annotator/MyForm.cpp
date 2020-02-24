b4f71b7a7f8ae76b6501c95b741551d34726538b
BuildTools <unconfigured@null.spigotmc.org>
2020-02-24 23:37:02 +0000

    Added basic UI

---
+++ Source Code/Image Annotator/Image Annotator/MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Image_Annotator::MyForm form;
+	Application::Run(%form);
+}
