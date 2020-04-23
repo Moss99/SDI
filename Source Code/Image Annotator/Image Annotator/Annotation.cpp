6b91f1b9eef1c91da4a6b708af360c9bb2add17a
risk08 <terrathorn25@gmail.com>
2020-04-23 16:48:19 +0100

    hdf5 lib

---
+++ Source Code/Image Annotator/Image Annotator/Annotation.cpp
@@ -0,0 +1,19 @@
+#include "Annotation.h"
+
+void annotation::save() {
+    //using namespace HighFive;
+    //// we create a new hdf5 file
+    //File file("/tmp/new_file.h5", File::ReadWrite | File::Create | File::Truncate);
+
+    //std::vector<int> data(50, 1);
+
+    //// let's create a dataset of native integer with the size of the vector 'data'
+    //DataSet dataset = file.createDataSet<int>("/dataset_one", DataSpace::From(data));
+
+    //// let's write our vector of int to the HDF5 dataset
+    //dataset.write(data);
+
+    //// read back
+    //std::vector<int> result;
+    //dataset.read(result);
+}
---
+++ Source Code/Image Annotator/Image Annotator/Annotation.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "H5Cpp.h"
+#include <string>
+
+class annotation {
+public:
+	void save();
+};
