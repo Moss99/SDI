2f81309a45074eb379223609372cce2cec2ec762
risk08 <terrathorn25@gmail.com>
2020-04-25 10:08:37 +0100

    fixed save

--- Source Code/Image Annotator/Image Annotator/Annotation.cpp
+++ Source Code/Image Annotator/Image Annotator/Annotation.cpp
@@ -9,15 +9,33 @@ const std::string FILE_NAME("save.annotations");
 const std::string DATASET_NAME("dset");
 
 void Annotation::save(std::vector<std::vector<int>> &shapes) {
+    std::vector<int> flat;
+    for (int x = 0; x < shapes.size(); x++) { //flattern out vector
+        for (int y = 0; y < shapes[x].size(); y++) {
+            flat.push_back(shapes[x][y]);
+        }
+        flat.push_back(-1); //denote new shape (assumes -ve is safe to use)
+    }
     File file(FILE_NAME, File::ReadWrite | File::Create | File::Truncate);
-    DataSet dataset = file.createDataSet<int>(DATASET_NAME, DataSpace::From(shapes));
-    dataset.write(shapes);
+    DataSet dataset = file.createDataSet<int>(DATASET_NAME, DataSpace::From(flat));
+    dataset.write(flat);
 }
 
 std::vector<std::vector<int>> Annotation::open(std::string path) {
     File file(path, File::ReadOnly);
-    std::vector<std::vector<int>> readData;
+    std::vector<int> readData;
     DataSet dataset = file.getDataSet(DATASET_NAME);
     dataset.read(readData);
-    return readData;
+    std::vector<std::vector<int>> unflat;
+    std::vector<int> points;
+    for (int i = 0; i < readData.size(); i++) { //un-flattern vector
+        if (readData[i] == -1) {
+            unflat.push_back(points);
+            points.clear();
+        }
+        else {
+            points.push_back(readData[i]);
+        }
+    }
+    return unflat;
 }
